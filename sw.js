3ac4e4993b02765e53b2105b7437925be84e4d70
goodhousepp <goodhousepp@gmail.com>
2026-02-14 11:39:58 +0900

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE_NAME = "lista-compras-gp-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+const CACHE_NAME = "comprafacil-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
@@ -17,9 +26,17 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached ||
-      fetch(event.request).catch(() => caches.match("./index.html"))
-    )
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((resp) => {
+            const copy = resp.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+            return resp;
+          })
+          .catch(() => caches.match("./index.html"))
+      );
+    })
   );
 });
